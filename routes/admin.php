353de3a2625fe5a9a6c0abc3d54b51aa0478652a
Ravshan <ravshan488889@gmail.com>
2024-11-23 15:28:00 +0500

    crud mark-types

--- routes/admin.php
+++ routes/admin.php
@@ -1,11 +1,14 @@
 <?php
+use App\Http\Controllers\Admin\MarkTypeController;
 
-Route::resource('galleries', GalleryController::class);
-    Route::get('admin/sample', function(){
+Route::get('admin/sample', function(){
     return view('admin.sample');
-}
-);
+});
 
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::resource('mark-types', MarkTypeController::class);
+});
 //Route::middleware('auth')->group(function () {
 //    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 //    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\GalleryController;
 use Illuminate\Support\Facades\Route;
 
 //use App\Http\Controllers\Admin\GalleryCategoryController;
