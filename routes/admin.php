31ded639568b486ce14b8ade905f54fb41a41d5d
mohamedjn008 <ibragimovmuhammad@gmail.com>
2024-11-28 16:11:55 +0500

    times-crud

--- routes/admin.php
+++ routes/admin.php
@@ -4,16 +4,16 @@ use App\Http\Controllers\Admin\FeedbackController;
 use App\Http\Controllers\Admin\CompetitionController;
 use App\Http\Controllers\Admin\MarkTypeController;
 use App\Http\Controllers\Admin\TimesController;
-  
+
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::resource('mark-types', MarkTypeController::class);
     Route::resource('competitions', CompetitionController::class);
     Route::resource('feedbacks', FeedbackController::class);
     Route::resource('times', TimesController::class);
 
- 
+
 });
- 
+
 //Route::middleware('auth')->group(function () {
 //    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 //    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
