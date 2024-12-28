dea5b430be0fbf2d71c7dcbd916145c611086205
rvositov <ravshan488889@gmail.com>
2024-12-28 15:27:06 +0500

    public-site blade-sample

---
+++ resources/views/layouts/slider.blade.php
@@ -0,0 +1,23 @@
+<!-- banner-area -->
+<section class="banner-area fix p-relative">
+    <div class="banner-bg banner-bg-rainbow" data-background="assets/img/banner/banner-bg-2.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-8">
+                    <div class="hero-content hero-content-black">
+                        <h2 class="hero-title-black mb-45">Online Learning Designed For <br>Real Life</h2>
+                        <div class="hero-btn">
+                            <a href="course-list.html" class="tp-btn">All Courses</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="banner-shape d-none d-md-block">
+                        <img src="assets/img/banner/banner-img-1.png" alt="banner-shape" class="b-shape-3">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- banner-area-end -->
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\PositionsController;
+use App\Http\Controllers\Public\GalleryController;
+use App\Http\Controllers\Public\MainController;
 use Illuminate\Support\Facades\Route;
 
 //use App\Http\Controllers\Admin\GalleryCategoryController;
@@ -10,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
 
-Route::get('/', function () {
-    return view('layouts.app');
+Route::get('/', [MainController::class, 'index'] )->name('home') ;
+Route::get('/gallery', [GalleryController::class, 'index'] )->name('gallery') ;
+
+Route::get('/welcome', function () {
+    return view('welcome');
 });
